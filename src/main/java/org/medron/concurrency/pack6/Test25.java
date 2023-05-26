8c042e03b9f5a3386baccda971762e65401a0c8e
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-05-26 13:45:11 +0300

    Concurrency #6

---
+++ src/main/java/org/medron/concurrency/pack6/Test25.java
@@ -0,0 +1,13 @@
+package org.medron.concurrency.pack6;
+@SuppressWarnings({"java:S106","java:S2276","java:S2274","java:S2446","java:S2142","java:S1130","java:S1602","java:S1481","java:S112","java:S1854"})
+
+public class Test25 {
+    public static void main(String[] args) {
+        int a = 5;
+        //System.out.println("a++: "+ a++ + " "+ a);
+        System.out.println("++a: "+ ++a + " "+ a);
+
+        System.out.println(a);
+
+    }
+}
